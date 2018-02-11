870486bf89d593433630fc8a45457cae590ab937
NickStrach <strachn@wwu.edu>
2018-02-11 08:36:49 -0800

    Cleaning up gui and getting ready to combine with story generation

--- main.py
+++ main.py
@@ -83,19 +83,25 @@ class FullScreenApp(object):
             master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
         master.bind('<Escape>',self.toggle_geom)
 
-        master.title("Imagination Unleashed")
-        self.label = Label(master, text="Welcome to Imagination Unleashed! Please select the type of story you'd like!")
+        master.title("Pure Imagination Unleashed")
+        self.label = Label(master, text="Welcome to Pure Imagination Unleashed! Please select the type of story you'd like!")
         self.label.pack()
 
+        # Generate drop down to select type
         types = ["alice and wonderland", "harry potter", "pirates"]
+        variable = StringVar(master)
+        variable.set(types[0])
+
+        menu = OptionMenu(master, variable, types[0], types[1], types[2])
+        menu.pack()
+
+        '''
         for type in types:
             self.type = Button(master, text=type, command=self.start)
             self.type.pack()
+        '''
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
-
-        self.start_button = Button(master, text="Go!", command=self.start)
+        self.start_button = Button(master, text="Go!", command=start_storytime)
         self.start_button.pack()
 
         image = getImage("santa")
@@ -105,6 +111,11 @@ class FullScreenApp(object):
         self.panel.place(x=700, y=300)
         self.panel.configure(image = img)
         self.panel.image = img
+
+        '''
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack(side=RIGHT, padx=800, pady=50)
+        '''
 
     def switch_picture(self, search_term):
         print("switching")
@@ -130,52 +141,16 @@ app=FullScreenApp(root)
 root.mainloop()
 #app.switch_picture(search_term)
 
-file = open('generated_story.txt', 'r').readlines()
+def start_storytime():
 
-for sentence in file:
-    display_picture(sentence, app)
-    text2Speech(sentence[1:-3]) #Reads the sentence outloud
-    time.sleep(1)
+    #Call the function to generate a story
 
-'''
-def url_to_image(url):
-    resp = urllib.request.urlopen(url)
-    image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    #image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    image = Image.fromarray(image, 'RGB')
+    file = open('generated_story.txt', 'r').readlines()
 
-    return image
-
-
-
-
-FLICKR_PUBLIC = '6fdf088b862a49a3ae1e678296c00936'
-FLICKR_SECRET = '605ede6b9004a09e'
-
-flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
-extras='url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o'
-
-cats = flickr.photos.search(tags='grass', per_page=5, extras=extras)
-
-photos = cats['photos']
-from pprint import pprint
-pprint(photos)
-
-photoUrl = photos['photo'][0]['url_m']
-
-response = requests.get(photoUrl)
-image = Image.open(BytesIO(response.content))
-
-
-
-#image = url_to_image(photoUrl)
-
-#cv2.imshow("Image", image)
-#cv2.waitKey(0)
-
-#image.show()
-
-'''
+    for sentence in file:
+        display_picture(sentence, app)
+        text2Speech(sentence[1:-3]) #Reads the sentence outloud
+        time.sleep(1)
 
 
 ''' Text to speech stuff
